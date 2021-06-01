888e7b111ab22b72ce7f0c0d15a4c1e984e6f9ea
MD. Abdur Rahman <freelancerrahman0@gmail.com>
2021-06-02 04:36:35 +0600

    secend time

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,10 @@ $(function () {
 
     // portfolio js ends here
 
+    $(window).on('load', function () {
+        $('.preloader').delay(2000).fadeOut(2000);
+    });
+
 
     $('.banner-slider').slick({
         arrows: false,
@@ -16,15 +20,15 @@ $(function () {
         dots: true,
     });
 
-    $('.team-slider').slick({
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        dots: true,
-    });
+    // $('.team-slider').slick({
+    //     infinite: true,
+    //     slidesToShow: 4,
+    //     slidesToScroll: 1,
+    //     arrows: false,
+    //     autoplay: true,
+    //     autoplaySpeed: 1500,
+    //     dots: true,
+    // });
     $('.slider-client').slick({
         infinite: true,
         slidesToShow: 4,
@@ -34,6 +38,53 @@ $(function () {
         autoplaySpeed: 1500,
         dots: true,
     });
+    $('.team-slider').slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true,
+              arrows: false,
+            }
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true,
+              arrows: false,
+            }
+          },
+          {
+            breakpoint: 769,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              arrows: false,
+            }
+          },
+          {
+            breakpoint: 575,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows: false,
+            }
+          }
+        ]
+      });
 
     $('.slider').slick({
         arrows: false,
